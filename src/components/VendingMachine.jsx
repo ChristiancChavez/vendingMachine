09b735a631c416e2703c280c2747f4f3fb5371db
ChristiancChavez <32717897+ChristiancChavez@users.noreply.github.com>
2023-10-27 17:29:26 -0500

    add button to handle retur coin

--- src/components/VendingMachine.jsx
+++ src/components/VendingMachine.jsx
@@ -3,46 +3,59 @@ import MethodButton from '../components/MethodButton';
 
 const VendingMachine = () => {
 
-  const [availableItems, setAvailableItems] = useState([
-    { name: 'Water', price: 0.65, count: 5 },
-    { name: 'Juice', price: 1.00, count: 3 },
-    { name: 'Soda', price: 1.50, count: 7 },
-  ]);
-  const [availableChange, setAvailableChange] = useState({
-    0.05: 10,
-    0.10: 10,
-    0.25: 10,
-    1: 10,
-  });
+    const [availableItems, setAvailableItems] = useState([
+        { name: 'Water', price: 0.65, count: 5 },
+        { name: 'Juice', price: 1.00, count: 3 },
+        { name: 'Soda', price: 1.50, count: 7 },
+    ]);
+    const [availableChange, setAvailableChange] = useState({
+        0.05: 10,
+        0.10: 10,
+        0.25: 10,
+        1: 10,
+    });
 
-  const [insertedMoney, setInsertedMoney] = useState(0);
-  const [selectedItem, setSelectedItem] = useState(null);
+    const [insertedMoney, setInsertedMoney] = useState(0);
+    const [selectedItem, setSelectedItem] = useState(null);
 
-  const handleSelectItem = (item) => {
-    if (insertedMoney >= item.price && item.count > 0) {
-      setSelectedItem(item);
-      setInsertedMoney(insertedMoney - item.price);
-      const updatedItems = availableItems.map((i) =>
-        i.name === item.name ? { ...i, count: i.count - 1 } : i
-      );
-      setAvailableItems(updatedItems);
-    }
-  };
+    const handleSelectItem = (item) => {
+        if (insertedMoney >= item.price && item.count > 0) {
+        setSelectedItem(item);
+        setInsertedMoney(insertedMoney - item.price);
+        const updatedItems = availableItems.map((i) =>
+            i.name === item.name ? { ...i, count: i.count - 1 } : i
+        );
+        setAvailableItems(updatedItems);
+        }
+    };
 
+    const handleReturnCoin = () => {
+        setInsertedMoney(0);
+    };
+    
 
-  return (
-    <div>
-        <h1>Vending Machine</h1>
+    return (
         <div>
-        {availableItems.map((item) => (
-          <div key={item.name}>
-            <MethodButton text={item.name} disabled="disabled" onClick={handleSelectItem} />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-
+            <h1>Vending Machine</h1>
+            <div>
+            {availableItems.map((item) => (
+            <div key={item.name}>
+                <MethodButton 
+                    text={item.name} 
+                    disabled={insertedMoney < item.price || item.count === 0} 
+                    onClick={handleSelectItem} 
+                />
+            </div>
+            ))}
+            </div>
+            <div>
+                <MethodButton 
+                    onClick={handleReturnCoin} 
+                    text="Return Coin" 
+                />
+            </div>
+        </div>
+    )
 };
 
 export default VendingMachine;
