214d25a44eaa30c11e7b39200d9a645ff25a9f8c
ChristiancChavez <32717897+ChristiancChavez@users.noreply.github.com>
2023-10-27 19:40:20 -0500

    change calculateChange to calculate the changes in coins

--- src/components/VendingMachine.jsx
+++ src/components/VendingMachine.jsx
@@ -8,6 +8,7 @@ const VendingMachine = () => {
         { name: 'Juice', price: 1.00, count: 3 },
         { name: 'Soda', price: 1.50, count: 7 },
     ]);
+
     const [availableChange, setAvailableChange] = useState({
         0.05: 10,
         0.10: 10,
@@ -20,7 +21,7 @@ const VendingMachine = () => {
         { label: 'Insert $0.10', value: 0.10 },
         { label: 'Insert $0.25', value: 0.25 },
         { label: 'Insert $1', value: 1 },
-      ];
+    ];
 
     const [insertedMoney, setInsertedMoney] = useState(0);
     const [selectedItem, setSelectedItem] = useState(null);
@@ -47,29 +48,48 @@ const VendingMachine = () => {
     const calculateChange = (changeAmount) => {
         const change = {};
         const availableCoins = [1, 0.25, 0.10, 0.05];
-    
-        for (const coin of availableCoins) {
-            const count = Math.floor(changeAmount / coin);
-            if (count > 0) {
-                change[coin] = count;
-                changeAmount -= count * coin;
+        
+        // Create a copy of the availableChange state to avoid directly modifying it
+        const updatedAvailableChange = { ...availableChange };
+        
+            for (const coin of availableCoins) {
+                if (changeAmount >= coin && updatedAvailableChange[coin] > 0) {
+                    const count = Math.floor(changeAmount / coin);
+                    const numCoinsToUse = Math.min(count, updatedAvailableChange[coin]);
+                    
+                    change[coin] = numCoinsToUse;
+                    changeAmount -= numCoinsToUse * coin;
+            
+                    // Update the availableChange state
+                    updatedAvailableChange[coin] -= numCoinsToUse;
+                }
             }
-        }
-    
-        return change;
-    };
+        
+            // Update the availableChange state with the modified object
+            setAvailableChange(updatedAvailableChange);
+        
+            return change;
+        };
 
     const vendItem = () => {
         if (selectedItem) {
+            const change = calculateChange(insertedMoney - selectedItem.price);
+
             return (
                 <div>
-                <p>{selectedItem.name}</p>
-                {calculateChange(insertedMoney - selectedItem.price)}
+                    <p>{selectedItem.name}</p>
+                    <p>Change:</p>
+                    <ul>
+                        {Object.entries(change).map(([coin, count]) => (
+                            <li key={coin}>
+                            {count} {coin}-dollar coin{count > 1 ? 's' : ''}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             );
-            }
-        };
-    
+        }
+    };
 
     return (
         <div>
